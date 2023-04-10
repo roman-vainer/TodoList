8bf799ab91deca2f4876ee4d1a139e71dc480325
Roman Vainer <7019267@gmail.com>
2023-04-10 14:51:10 +0200

    tcp H2 port 9092

--- src/main/java/ua/shpp/todolist/TodoListApplication.java
+++ src/main/java/ua/shpp/todolist/TodoListApplication.java
@@ -1,7 +1,12 @@
 package ua.shpp.todolist;
 
+import org.h2.tools.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+
+import java.sql.SQLException;
 
 @SpringBootApplication
 public class TodoListApplication {
